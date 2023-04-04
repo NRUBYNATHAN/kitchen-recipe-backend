257a1775a82c8fbe1cff7f4fa4c8b32cc6268bf3
NRUBYNATHAN <rubynathan999@gmail.com>
2023-04-04 22:49:32 +0530

    cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const client = new MongoClient(MONGO_URL); // dial
 // Top level await
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
